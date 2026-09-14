f02314fc588775ccc44661c14b3f7e209a2b80fc
Aditya Raj Gupta <adityarajgupta13042006@gmail.com>
2026-09-14 12:39:59 +0530

    Add new solutions for Rectangle Overlap and Rectangle Area problems

---
+++ Practise/2026/Third Quarter/Week-7/28)223-Rectangel_Area.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+class Solution {
+public:
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1,
+                    int bx2, int by2) {
+        int area1 = (ax2 - ax1) * (ay2 - ay1);
+        int area2 = (bx2 - bx1) * (by2 - by1);
+        int breadth = 0;
+        int height = 0;
+        if (ax2 > bx1 && ay2 > by1 && bx2 > ax1 && by2 > ay1) {
+            breadth = min(ax2, bx2) - max(ax1, bx1);
+            height = min(ay2, by2) - max(ay1, by1);
+        }
+        return area1 + area2 - (breadth * height);
+    }
+};
